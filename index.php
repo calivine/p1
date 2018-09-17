2227fb18df906e6ef7a4277fdf55693f30154670
Alex Caloggero <a.calog@protonmail.com>
2018-09-16 21:07:43 -0400

    Updated index.php; added logic.php; updated image size

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require('logic.php'); ?>
 <!DOCTYPE html>
 
 <html lang="en">
@@ -10,7 +11,7 @@
   <body>
     <div class="container">
       <h1>Alex Caloggero</h1>
-      <img>
+      <img src='./images/ac.jpg' alt='Alex Caloggero'>
       <h2>About Me</h2>
       <p>I'm Alex Caloggero. I'm from just outside of Boston, MA and currently work as a prospect researcher at Tufts University. About a year ago I 
 took the online course, CS50x, which is Harvard's intro to Computer science course. I discovered that I really loved programming and design thinking and 
@@ -19,6 +20,7 @@ and have also worked on my own personal projects, learning and
 absorbing all that I can. I want to make a shift career-wise towards software development and this is my first big step! Great to meet you all and 
 excited to get started!</p>
       <h2>Random Quote</h2>
+        <blockquote><em><?php echo $quote[$index];?></em></blockquote>
     </div>
   </body>
 
---
+++ logic.php
@@ -0,0 +1,7 @@
+<?php
+$quote = [
+    'We are what we repeatedly do. Excellence, then, is not an act, but a habit. -Aristotle',
+    'Sorrow can be alleviated by good sleep, a bath and a glass of wine. -Thomas Aquinas',
+    'Young man, in mathematics you dont understand things. You just get used to them. -John von Neumann'
+];
+$index = rand(0, 2);
